a5129c07ce629044fe0b9d6036b11bc6a508e08a
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2018-05-28 11:00:39 +0200

    * gene id handling * use Ensembl for RNA Seq to avoid problems with duplicate/nonsense HGNC IDs * use Entrez ID for CNV (same reason)

--- src/dream_challenge_data.jl
+++ src/dream_challenge_data.jl
@@ -10,8 +10,6 @@ function import_dream_challenge_data(directory::String)
 		key_type = Gene
 		if data_type_string == "Exomeseq"
 			data_type = ExomeSeq
-			# problem: if not Type is different from Missense, the summary does not exist
-			key_type = Tuple{Gene, String}
 		elseif data_type_string == "GeneExpression"
 			data_type = GeneExpression
 		elseif data_type_string == "Methylation"
@@ -36,6 +34,7 @@ function import_dream_challenge_data(directory::String)
 			df = CSV.read(joinpath(directory, f), DataFrame, delim='\t',rows_for_type_detect=1200, null="NA", weakrefstrings=false)
 		end
 		info("creating genes or proteins...")
+		info("number of genes so far: Entrez: $(length(experiment.genes)), HGNC: $(length(experiment.genes_by_hgnc)), Ensembl: $(length(experiment.genes_by_ensembl))")
 		if data_type_string == "Drug_Response_Training"
 			drugs = names(df)[2:end]
 			cell_lines = df[:CellLine].values
@@ -47,29 +46,7 @@ function import_dream_challenge_data(directory::String)
 				add_outcome!(experiment, Drug(string(d)), o)
 			end
 			continue
-		elseif data_type_string != "RPPA"
-			# all_genes = df[:HGNC_ID]
-			is_cancer_gene = false
-			i = 1
-			n = size(df, 1)
-			for data_row in eachrow(df)
-				gene_object = add_gene(experiment, data_row[:HGNC_ID].value, is_cancer_gene = is_cancer_gene)
-				if in(Symbol("CancerGene?"), names(df))
-					is_cancer_gene = data_row[Symbol("CancerGene?")].value == "No" ? false : true
-					gene_object.cancer_gene = is_cancer_gene
-				end
-				if in(:Ensembl_ID, names(df))
-					e_id = data_row[:Ensembl_ID].value
-					add_gene_id!(experiment, gene_object, e_id, "ensembl_id")
-				end
-				if in(:EntrezID, names(df))
-					e_id = data_row[:EntrezID].value
-					add_gene_id!(experiment, gene_object, e_id)
-				end
-				i+=1
-				if i%5000 == 0 info("processed $i of $n genes...") end
-			end
-		else
+		elseif data_type_string == "RPPA"
 			i = 1
 			all_proteins = df[[:Antibody_ID, :FullyValidated]]
 			n = size(all_proteins, 1)
@@ -79,19 +56,111 @@ function import_dream_challenge_data(directory::String)
 			end
 			i += 1
 			if i%5000 == 0 info("processed $i of $n proteins...") end
+		else
+			i = 1
+			n = size(df, 1)
+			df_names = names(df)
+			for data_row in eachrow(df)
+				gene_object = nothing
+				has_entrez_id = false
+				has_hgnc_id = false
+				has_ensembl_id = false
+				entrez_id = -1
+				ensembl_id = ""
+				hgnc_id = ""
+
+				# check for different id types and try to retrieve gene object
+				if in(:EntrezID, df_names)
+					has_entrez_id = true
+					entrez_id = data_row[:EntrezID].value
+					try 
+						gene_object = get_gene(experiment, entrez_id)
+					end
+				end
+
+				if in(:Ensembl_ID, df_names)
+					has_ensembl_id = true
+					ensembl_id = data_row[:Ensembl_ID].value
+					if gene_object == nothing
+						try
+							gene_object = get_gene(experiment, ensembl_id, "ensembl_id")
+						end
+					else
+						add_gene_id!(experiment, gene_object, ensembl_id, "ensembl_id")
+					end
+				end
+
+				if in(:HGNC_ID, df_names)
+					has_hgnc_id = true
+					hgnc_id = data_row[:HGNC_ID].value
+					if gene_object == nothing
+						try
+							gene_object = get_gene(experiment, hgnc_id, "hgnc_id")
+						end
+					else
+						add_gene_id!(experiment, gene_object, hgnc_id, "hgnc_id")
+					end
+				end
+
+				if has_entrez_id
+					if gene_object == nothing
+						#no gene found for any id type
+						gene_object = add_gene(experiment, entrez_id)
+						if has_ensembl_id add_gene_id!(experiment, gene_object, ensembl_id, "ensembl_id") end
+						if has_hgnc_id add_gene_id!(experiment, gene_object, hgnc_id, "hgnc_id") end
+					else
+						# check if object had no entrez id before
+						if gene_object.entrez_id == -1 
+							add_gene_id!(experiment, gene_object, entrez_id, "entrez_id")
+						else
+							# found gene object has another entrez id (which is unique) so create a new gene
+							gene_object = add_gene(experiment, entrez_id)
+							if has_ensembl_id add_gene_id!(experiment, gene_object, ensembl_id, "ensembl_id") end
+							if has_hgnc_id add_gene_id!(experiment, gene_object, hgnc_id, "hgnc_id") end
+						end
+					end
+				end
+
+				if has_ensembl_id
+					if gene_object == nothing
+						# if still no gene object, no entrez id is present
+						gene_object = add_gene(experiment, ensembl_id, "ensembl_id")
+						if has_hgnc_id add_gene_id!(experiment, gene_object, hgnc_id, "hgnc_id") end
+					else
+						# ensembl ids also are uniqe
+						if gene_object.ensembl_id == ""
+							add_gene_id!(experiment, gene_object, ensembl_id, "ensembl_id")
+						else
+							gene_object = add_gene(experiment, ensembl_id, "ensembl_id")
+							if has_entrez_id add_gene_id!(experiment, gene_object, entrez_id, "entrez_id") end
+							if has_hgnc_id add_gene_id!(experiment, gene_object, hgnc_id, "hgnc_id") end
+						end
+					end
+				end
+
+				if has_hgnc_id
+					if gene_object == nothing
+						# if still no gene object, hgnc id is the only id present
+						gene_object = add_gene(experiment, hgnc_id, "hgnc_id")
+					else
+						if gene_object.hgnc_id != hgnc_id add_gene_id!(experiment, gene_object, hgnc_id, "hgnc_id") end
+					end
+				end
+
+				if in(Symbol("CancerGene?"), df_names)
+					is_cancer_gene = data_row[Symbol("CancerGene?")].value == "No" ? false : true
+					gene_object.cancer_gene = is_cancer_gene
+				end
+				i+=1
+				if i%5000 == 0 info("processed $i of $n genes...") end
+			end
 		end
+		info("number of genes now: Entrez: $(length(experiment.genes)), HGNC: $(length(experiment.genes_by_hgnc)), Ensembl: $(length(experiment.genes_by_ensembl))")
 		info("importing cell line data")
 		for cl in get_cell_line_names_from_data_frame(data_type, df)
 			cl_obj = get_cell_line(experiment, cl, "Breast cancer")
-			data_view = nothing
-			try
-				data_view = get_dataview(cl_obj, data_type)
-			catch KeyError
-				data_view = DataView{key_type, data_type}(cl)
-			end
+			data_view = get_dataview!(cl_obj, data_type, DataView{key_type, data_type}(cl))
 			populate_data_view!(data_view, df, experiment)
-
-			add_dataview!(cl_obj, data_view)
 		end
 		add_view!(experiment, data_type)
 	end
@@ -101,12 +170,12 @@ end
 get_cell_line_names_from_data_frame(::Type{ExomeSeq}, df::DataFrame) = map(string, map(v->v.value, unique(df[:CellLine])))
 get_cell_line_names_from_data_frame(::Type{GeneExpression}, df::DataFrame) = extract_column_names(df, 2)
 get_cell_line_names_from_data_frame(::Type{Methylation}, df::DataFrame) = extract_column_names(df, 5)
-get_cell_line_names_from_data_frame(::Type{Union{RNASeq, RPPA, CNV}}, df::DataFrame) = extract_column_names(df, 3)
+get_cell_line_names_from_data_frame(::Type{<:Union{RNASeq, RPPA, CNV}}, df::DataFrame) = extract_column_names(df, 3)
 
 extract_column_names(df::DataFrame, first_used_column::Int64) = map(string, names(df)[first_used_column:end])
 
-function populate_data_view!(data_view::DataView{Tuple{Gene, String},ExomeSeq}, df::DataFrame, experiment::Experiment)
-	cl_df = view(df, df[:CellLine] .== data_view.cell_line_id)
+function populate_data_view!(data_view::DataView{Gene,ExomeSeq}, df::DataFrame, experiment::Experiment)
+	cl_df = view(df, df[:CellLine].values .== data_view.cell_line_id)
 	for data_row in eachrow(cl_df)
 		gene = get_gene(experiment, data_row[:HGNC_ID].value)
 		num_cosmic = data_row[Symbol("#Cosmic")].value
@@ -129,7 +198,7 @@ function populate_data_view!(data_view::DataView{Tuple{Gene, String},ExomeSeq}, 
 		variant_dist3effective_avg = data_row[Symbol("DistanceEffective3'end(alt)")].value
 		details = data_row[:Details].value
 		# data_summary = reference_mismatch_avg + variant_mismatch_avg + reference_mismatch_sum + variant_mismatch_sum + reference_dist3effective_avg + variant_dist3effective_avg
-		key = (gene, protein_change)
+		
 		#create some exome data view, create a key type for exome sequencing as a combination
 		#of gene and protein change
 		# exome_data = ExomeSeq(num_cosmic, variant_effect, protein_change,
@@ -147,8 +216,7 @@ function populate_data_view!(data_view::DataView{Tuple{Gene, String},ExomeSeq}, 
 								norm_zygosity=norm_zygosity, norm_reference_count=norm_reference_count, norm_variant_count=norm_variant_count,
 								tumor_zygosity=tumor_zygosity, tumor_reference_count=tumor_reference_count, tumor_variant_count=tumor_variant_count,
 								details=details)
-
-		add_measurement!(data_view, key, exome_data)
+		add_measurement!(data_view, gene, exome_data)
 	end
 end
 
@@ -172,27 +240,19 @@ function populate_data_view!(data_view::DataView{Gene,Methylation}, df::DataFram
 end
 
 function populate_data_view!(data_view::DataView{Gene,RNASeq}, df::DataFrame, experiment::Experiment)
-	tic()
-	cl_df = df[[:HGNC_ID, Symbol(data_view.cell_line_id)]]
+	cl_df = df[[:Ensembl_ID, Symbol(data_view.cell_line_id)]]
 	for data_row in eachrow(cl_df)
-		gene = get_gene(experiment, data_row[1].value)
+		gene = get_gene(experiment, data_row[1].value, "ensembl_id")
 		is_quant = eltype(data_row[2].value) == Float64 ? false : true
-		try 
-			measurement = get_measurement(data_view, gene)
-			if is_quant
-				measurement.expression_status = data_row[2].value == 0 ? false : true
-			else
-				measurement.expression_value = data_row[2].value
-			end
-		catch KeyError
-			if is_quant
-				add_measurement!(data_view, gene, RNASeq(0.0, true))
-			else
-				add_measurement!(data_view, gene, RNASeq(data_row[2].value, false))
-			end
+		empty_measurement = RNASeq(0.0, false)
+		# this will return the existing measurment for the gene and spit out a warn message
+		measurement = get_measurement!(data_view, gene, empty_measurement)
+		if is_quant
+			measurement.expression_status = Bool(data_row[2].value)
+		else
+			measurement.expression_value = data_row[2].value
 		end
 	end
-	info("cell line $(data_view.cell_line_id) took $(toq()) seconds")
 end
 
 function populate_data_view!(data_view::DataView{Protein,RPPA}, df::DataFrame, experiment::Experiment)
@@ -205,7 +265,7 @@ function populate_data_view!(data_view::DataView{Protein,RPPA}, df::DataFrame, e
 end
 
 function populate_data_view!(data_view::DataView{Gene,CNV}, df::DataFrame, experiment::Experiment)
-	cl_df = df[[:HGNC_ID, Symbol(data_view.cell_line_id)]]
+	cl_df = df[[:EntrezID, Symbol(data_view.cell_line_id)]]
 	for data_row in eachrow(cl_df)
 		if !data_row[2].hasvalue continue end
 		gene_level_cnv = CNV(data_row[2].value)
