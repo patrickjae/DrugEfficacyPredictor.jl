cdc363269373a3aab0eff797e45b7d4513c3d284
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2018-05-28 10:58:22 +0200

    normalization and general data handling

--- src/data_handling.jl
+++ src/data_handling.jl
@@ -14,31 +14,7 @@ function get_cell_line(experiment::Experiment, data::Dict{String, Any})
 	end
 	get_cell_line(experiment, data["id"], data["cancer_type"])
 end
-######### VIEWS ###########
-add_view!(experiment::Experiment, view::Type{<:ViewType}) = union!(experiment.views, [view])
 
-
-function normalize_data_views(experiment::Experiment)
-	for v in experiment.views
-		all_values = Float64[]
-		#collect all values
-		for cl in values(experiment.cell_lines)
-			if haskey(cl.views, v)
-				all_values.push!(all_values, map(get_measurement_value, collect(values(cl.views[v]))))
-			end
-		end
-		view_mean = mean(all_values)
-		view_std = stdm(all_values, view_mean)
-		for cl in values(experiment.cell_lines)
-			if haskey(cl.views, v)
-				f = (x) -> (x-view_mean)/view_std
-				measurements = collect(values(cl.views[v]))
-				norm_vals = map(f, map(get_measurement_value, measurements))
-				map((m, nv) -> set_normalized_value(m, nv), zip(measurements, norm_vals))
-			end
-		end
-	end
-end
 
 ######### GENES ###########
 function add_gene(experiment::Experiment, gene::Gene) 
@@ -68,21 +44,26 @@ function add_gene(experiment::Experiment, data::Dict{String, Any})
 	is_cancer_gene = get(data, "is_cancer_gene", false)
 	add_gene(experiment, gene_id, id_type, is_cancer_gene = is_cancer_gene)
 end
+
 function add_gene(experiment::Experiment, gene_id::Int64; is_cancer_gene::Bool=false)
 	get!(experiment.genes, gene_id, Gene(gene_id, is_cancer_gene))
 end
 
 function add_gene_id!(experiment::Experiment, gene::Gene, id::String, id_type::String)
 	if id_type == "hgnc_id"
-		if (gene.hgnc_id != "") && (gene.hgnc_id != id) warn("overwriting HGNC_ID, new value is $(id), was $(gene.hgnc_id) before") end
+		if (gene.hgnc_id != "") && (gene.hgnc_id != id) 
+			warn("overwriting HGNC_ID, new value is $(id), was $(gene.hgnc_id) before") 
+		end
 		gene.hgnc_id = id
 		experiment.genes_by_hgnc[id] = gene
 	elseif id_type == "ensembl_id"
-		if (gene.ensembl_id != "") && (gene.ensembl_id != id) warn("overwriting Ensembl ID, new value is $(id), was $(gene.ensembl_id) before") end
+		if (gene.ensembl_id != "") && (gene.ensembl_id != id) 
+			warn("overwriting Ensembl ID, new value is $(id), was $(gene.ensembl_id) before (other IDs: $(gene.hgnc_id) (HGNC), $(gene.entrez_id) (Entrez))") 
+		end
 		gene.ensembl_id = id
 		experiment.genes_by_ensembl[id] = gene
 	else
-		warn("unknown genen id type $id_type, ignoring")
+		warn("unknown gene id type $id_type, ignoring")
 	end
 end
 
@@ -92,12 +73,13 @@ function add_gene_id!(experiment::Experiment, gene::Gene, entrez_id::Int64, id_t
 end
 
 # get a gene object
-function get_gene(experiment::Experiment, gene_id::String, id_type::String="hgnc_id") 
+function get_gene(experiment::Experiment, gene_id::String, id_type::String="hgnc_id")
 	if id_type == "hgnc_id"
 		return experiment.genes_by_hgnc[gene_id]
 	elseif id_type == "ensembl_id"
 		return experiment.genes_by_ensembl[gene_id]
 	end
+	throw(KeyError("key \"$gene_id\" not found for id_type \"$id_type\""))
 end
 
 get_gene(experiment::Experiment, gene_id::Int64, id_type::String="entrez_id") = experiment.genes[gene_id]
@@ -121,57 +103,116 @@ function add_protein(experiment::Experiment, data::Dict{String, Any})
 	get_protein(experiment, hgnc_id, fully_validated=ab_validated)
 end
 
+######### VIEWS ###########
+add_view!(experiment::Experiment, view::Type{<:ViewType}) = union!(experiment.views, [view])
+
 ######### DATA VIEWS ###########
-# add a data view to the cell line
-add_dataview!(cl::CellLine, d::DataView{T,D}) where {T,D} = cl.views[D] = d
-add_dataview(cl::CellLine, d::DataView{T,D}) where {T,D}  = cl.views[D] = d
 
 # get a data view for a certain data type from a cell line or create if not present
 get_dataview(cl::CellLine, data_type::Type) = cl.views[data_type]
-
+get_dataview!(cl::CellLine, data_type::Type, dv::DataView{<:KeyType, <:ViewType}) = get!(cl.views, data_type, dv)
 
 ######### MEASUREMENTS ###########
 # add a measurement, i.e. a gene/protein data pair to a data view
-function add_measurement(d::DataView{T,D}, subject::T, measurement::D) where {T,D}
-	try
-		measurement = get_measurement(d, subject)
-	catch KeyError
+function add_measurement(d::DataView{<:KeyType,<:ViewType}, subject::KeyType, measurement::ViewType)
+	if subject ∉ d.used_keys
 		d.measurements[subject] = measurement
+		union!(d.used_keys, [subject])
 		return d
 	end
 	warn("measurement already exists for $subject, ignoring...")
 	warn("if you want to force the new value, use add_measurement! instead")
 	d
 end
+	
+function add_measurement!(d::DataView{<:KeyType, <:VectorViewType}, subject::KeyType, measurement::VectorViewType)
+	union!(d.used_keys, [subject])
+	insert!(d.measurements, subject, measurement)
+end
 
-function add_measurement!(d::DataView{T,D}, subject::T, measurement::D) where {T,D}
+function add_measurement!(d::DataView{<:KeyType,<:SingleViewType}, subject::KeyType, measurement::SingleViewType)
+	union!(d.used_keys, [subject])
 	d.measurements[subject] = measurement
 	d
 end
 
-get_measurement(d::DataView{T,<:ViewType}, subject::T) where {T} = d.measurements[subject]
-get_measurement_value(d::DataView{T, <:ViewType}, subject::T) where {T} = get_measurement_value(d.measurements[subject])
+function get_measurement!(d::DataView{<:KeyType, <:SingleViewType}, subject::KeyType, measurement::SingleViewType)
+	union!(d.used_keys, [subject])
+	get!(d.measurements, subject, measurement)
+end
+
+get_measurement(d::DataView{T,<:SingleViewType}, subject::T) where {T<:KeyType} = d.measurements[subject]
+
+get_measurement_value(d::DataView{T, <:ViewType}, subject::T) where {T<:KeyType} = get_measurement_value.(d.measurements[subject])
+
+get_measurement_value(ds::Vector{<:ViewType}) = get_measurement_value.(ds)
 
 function get_measurement_value(d::ExomeSeq)
-	(d.reference_mismatch_avg + d.variant_mismatch_avg + d.reference_mismatch_sum 
-		+ d.variant_mismatch_sum + d.reference_dist3effective_avg + d.variant_dist3effective_avg)
+	(d.reference_mismatch_sum + d.variant_mismatch_sum)
 end
 
 get_measurement_value(d::GeneExpression) = d.expression_value
 get_measurement_value(d::Methylation) = d.illumina_beta_value
-get_measurement_value(d::RNASeq) = (d.expression_value, d.expression_status)
+get_measurement_value(d::RNASeq) = d.expression_value
 get_measurement_value(d::RPPA) = d.protein_abundance
 get_measurement_value(d::CNV) = d.gene_level_cnv
 
 
 ######### NORMALIZATION ###########
-function set_normalized_value(d::T, value::Float64) where {T <: ViewType}
+
+function normalize_data_views(experiment::Experiment)
+	#for each of the views
+	for v in experiment.views
+		# if v == ExomeSeq continue end
+		# reset the normalization statistics for this view
+		experiment.statistics[v] = OrderedDict{KeyType, Tuple{Float64, Float64}}()
+		# collect the keys in this view over all cell lines that it
+		all_keys_in_view = Vector{KeyType}()
+		for cl in values(experiment.cell_lines)
+			# if the cell line has data for this view, collect the keys
+			if haskey(cl.views, v)
+				all_keys_in_view = union(all_keys_in_view, cl.views[v].used_keys)
+			end
+		end
+		#iterate over all keys (i.e. genes/proteins/gene-protein change) and normalize across cell lines
+		for key in all_keys_in_view
+			#collect values
+			all_values = Float64[]
+			for cl in values(experiment.cell_lines)
+				if haskey(cl.views,v)
+					if key ∈ cl.views[v].used_keys
+						push!(all_values, get_measurement_value(cl.views[v].measurements[key])...)
+					end
+				end
+			end
+			mean_val = mean(all_values)
+			std_val = stdm(all_values, mean_val)
+			#iterate over values again and normalize 
+			for cl in values(experiment.cell_lines)
+				if haskey(cl.views,v)
+					if key ∈ cl.views[v].used_keys
+						set_normalized_value(cl.views[v].measurements[key], (get_measurement_value(cl.views[v].measurements[key]) .- mean_val)./std_val)
+					end
+				end
+			end
+			# store statistics
+			experiment.statistics[v][key] = (mean_val, std_val)
+		end
+	end
+	experiment.is_normalized = true
+end
+
+
+function set_normalized_value(d::ViewType, value::Float64)
 	d.normalized_value = value
 end
 
-get_normalized_value(d::DataView{T, D}, subject::T) where {T, D <: ViewType} = d.measurements[subject].normalized_value
-get_normalized_value(d::T) where {T <: ViewType} = d.normalized_value
+set_normalized_value(ds::Vector{<:VectorViewType}, values::Vector{Float64}) = foreach(pair -> set_normalized_value(pair[1], pair[2]), zip(ds, values))
 
+get_normalized_value(d::DataView{T, <:SingleViewType}, subject::T) where {T <: KeyType} = d.measurements[subject].normalized_value
+get_normalized_value(d::ViewType) = d.normalized_value
+get_normalized_value(ds::Vector{<:ViewType}) = get_normalized_value.(ds)
+get_normalized_value(d::DataView{<:KeyType, <:VectorViewType}, subject::KeyType) = map(e -> e.normalized_value, d.measurements[subject])
 
 
 ######### RESULTS ###########
