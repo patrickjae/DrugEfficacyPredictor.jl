064561504983007ba9b29dff922b8165f2961aaf
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2018-08-27 13:36:52 +0200

    asynchronous logging (experimental)

--- src/DrugEfficacyPredictor.jl
+++ src/DrugEfficacyPredictor.jl
@@ -9,13 +9,57 @@ include("webservice.jl")
 include("import_json.jl")
 include("model_initialization.jl")
 include("inference.jl")
+include("prediction.jl")
 
 """
 Adjust info and error functions to include time stamp.
 """
-info(message::String) = Base.info("\t[$(now())] $message")
-error(message::String) = Base.error("\t[$(now())] $message")
-warn(message::String) = Base.warn("[$(now())] $message")
+
+const info_messages = Channel{Tuple}(32)
+const warn_messages = Channel{Tuple}(32)
+const error_messages = Channel{Tuple}(32)
+
+"""
+Start the logging processes
+"""
+function start_info_logging()
+	for msg in info_messages
+		Base.info(msg..., prefix = "[$(now())]\t")
+	end
+end
+
+function start_warn_logging()
+	for msg in warn_messages
+		Base.warn(msg..., prefix = "[$(now())]\t")
+	end
+end
+
+function start_error_logging()
+	for msg in error_messages
+		try 
+			Base.error("[$(now())]\t", msg...)
+		catch exc
+			println(exc)
+		end
+	end
+end
+
+function start_logging()
+	@async start_info_logging()
+	@async start_warn_logging()
+	@async start_error_logging()
+end
+
+start_logging()
+
+import Base.info
+import Base.warn
+import Base.error
+
+info(message...) = put!(info_messages, message)
+warn(message...) = put!(warn_messages, message)
+error(message...) = put!(error_messages, message)
+
 
 
 
