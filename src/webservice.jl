7c7bc3e343e2e74623ac99b2e2d1f90fa6b12130
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2018-09-07 15:30:27 +0200

    use HTTP package

--- src/webservice.jl
+++ src/webservice.jl
@@ -1,5 +1,5 @@
 # server.jl
-using HttpServer, JSON
+using HTTP, JSON, Random
 
 # create a global variable to keep track of experiments in the server
 experiments_dictionary = Dict{String, DrugEfficacyPredictor.Experiment}()
@@ -7,60 +7,68 @@ experiments_dictionary = Dict{String, DrugEfficacyPredictor.Experiment}()
 # create an experiment for testing purposes
 experiments_dictionary["test_id"] = create_experiment()
 
-server = nothing
-function create_http_handler()
-	http = HttpHandler() do req::Request, res::Response
-    	response = handle_request(req)
-    	return Response(response)
-	end
-	http
-end
 
-function handle_request(req::Request)
-    if endswith(req.resource, "stop")
+function handle_request(req::HTTP.Request)
+    target = req.target
+    if endswith(target, "stop")
+        req.response.status = 200
+        req.response.body = JSON.json(Dict("status" => "success", "message" => "Server has been shut down."))
         close(server)
-        return JSON.json(Dict("status" => "success", "message" => "Server has been shut down."))
+        return req.response
     end
 
-	func = eval(parse(replace(req.resource, "/", "DrugEfficacyPredictor.", 1)))
+	func = eval(Meta.parse(replace(target, "/" => "DrugEfficacyPredictor.", count=1)))
     # if request wants to create an experiment return the id of the newly created object
-    if endswith(req.resource, "create_experiment")
+    if endswith(target, "create_experiment")
     	experiment = func()
-    	exp_id = string(Base.Random.uuid4())
+    	exp_id = string(Random.uuid4())
     	experiments_dictionary[exp_id] = experiment
-		return JSON.json(Dict("status" => "success", "experiment_id" => exp_id))
+        req.response.status = 200
+		req.response.body = JSON.json(Dict("status" => "success", "experiment_id" => exp_id))
+        return req.response
     end
 
     # for all other requests, require the experiment id.
-    request_dictionary = JSON.parse(transcode(String, req.data))
+    request_dictionary = JSON.parse(transcode(String, req.body))
     if !haskey(request_dictionary, "experiment_id")
-    	return JSON.json(Dict("status" => "error", "message" => "Please provide an experiment_id."))
+        req.response.status = 500
+    	req.response.body = JSON.json(Dict("status" => "error", "message" => "Please provide an experiment_id."))
+        return req.response
     end
     exp_id = request_dictionary["experiment_id"]
     if !haskey(experiments_dictionary, exp_id)
-    	return JSON.json(Dict("status" => "error", "message" => "Unknown experiment ID '$exp_id'."))
+        req.response.status = 500
+    	req.response.body = JSON.json(Dict("status" => "error", "message" => "Unknown experiment ID '$exp_id'."))
+        return req.response
     end
     experiment = experiments_dictionary[exp_id]
-    # try
+
+    try
     	func(experiment, request_dictionary)
-    	info("Request handled sucessfully, returning experiment $exp_id")
-		return JSON.json(Dict("status" => "success", "experiment_id" => exp_id))
- #    catch ex
- #    	st = map(string, stacktrace(catch_backtrace()))
- #    	error("Exception occurred: $(typeof(ex))")
- #    	error("Stacktrace:")
- #    	for st_entry in st
- #    		error(st)
- #    	end
- #    	error(string(e))
-	# 	return JSON.json(Dict("status" => "exception", "type" => string(ex), "stacktrace" => st))
-	# end
+        req.response.status = 200
+		req.response.body = JSON.json(Dict("status" => "success", "experiment_id" => exp_id))
+    catch ex
+    	st = map(string, stacktrace(catch_backtrace()))
+    	# error("Exception occurred: $(typeof(ex))")
+    	# @error "Stacktrace" st
+        req.response.status = 500
+		req.response.body = JSON.json(Dict("status" => "exception", "type" => string(ex), "stacktrace" => st))
+        return req.response
+	end
+    req.response
 end
 
+server = nothing
+
+create_http_handler() = HTTP.Handlers.HandlerFunction(handle_request)
+
+close(server::HTTP.Server) = put!(server.in, HTTP.Servers.KILL)
+
+
 function start_server(port::Int64)
-	global server = Server(create_http_handler())
+	global server = HTTP.Server(create_http_handler())
 	try 
-		run(server, port)
+		HTTP.serve(server, "127.0.0.1", port)
 	finally
 		close(server)
 	end	
