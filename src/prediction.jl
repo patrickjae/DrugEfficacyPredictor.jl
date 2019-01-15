82e41b6a074321fa3bb3f60c932cb624ee62277f
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-01-15 15:14:56 +0100

    compute cross kernels and predict with provided cell lines argument

--- src/prediction.jl
+++ src/prediction.jl
@@ -47,72 +47,51 @@ function predict_outcomes(dep::DrugEfficacyPrediction, m::PredictionModel,
             cell_lines::Vector{CellLine})
     predictions = Dict{Drug, Vector{Float64}}()
     ranks = Dict{Drug, Vector{Int64}}()
-    # all cell lines
-    all_cell_lines = collect(values(dep.experiment.cell_lines))
+    # all cell lines present for training
+    all_cell_lines = filter(cl -> !cl.in_test_set, collect(values(dep.experiment.cell_lines)))
 
-
-
-    # if base_kernels == nothing || pathway_specific_kernels == nothing
-    #     (K, base_kernels, pathway_specific_kernels) = compute_all_kernels(dep.experiment, training_cell_lines, cell_lines)
-
-    #     # base_kernels = Dict{Type{<:ViewType}, Matrix{Float64}}()
-    #     # for v in dep.experiment.views
-    #     #     dataviews_to_predict = map_data_views(cell_lines, v)
-    #     #     dataviews_from_training = map_data_views(training_cell_lines, v)
-    #     #     # dataviews_from_training = map(cl -> cl.views[v], collect(keys(dep.experiment.results[drug].outcome_values)))
-    #     #     k = compute_kernel(dataviews_from_training, dataviews_to_predict)
-    #     #     base_kernels[v] = k
-    #     #     # @info "computed kernels for view $v, num data views to predict: $(length(dataviews_to_predict)), data views in training: $(length(dataviews_from_training)), size of kernel: $(size(k))"
-    #     # end
-    # end
+    (num_cross_kernels, base_cross_kernels, pw_specific_cross_kernels) = compute_all_kernels(dep.experiment, all_cell_lines, cell_lines, subsume_pathways = dep.subsume_pathways)
     # do predictions for all drugs we saw at training time
     for (t, drug) in enumerate(keys(dep.experiment.results))
-        # find cell lines that were used for training
-        training_cell_lines = filter(cl -> !cl.in_test_set && haskey(dep.experiment.results[drug].outcome_values, cl), all_cell_lines)
-
+        # find cell lines that have outcome for this drug
+        training_cell_lines = filter(cl -> haskey(dep.experiment.results[drug].outcome_values, cl), all_cell_lines)
         training_set_cell_line_idx = findall((in)(training_cell_lines), all_cell_lines)
-        predict_cell_line_idx = findall((in)(cell_lines), all_cell_lines)
+        # find the ids of these cell_lines
+        # training_set_cell_line_idx = findall((in)(training_cell_lines), all_cell_lines)
+        # predict_cell_line_idx = findall((in)(cell_lines), all_cell_lines)
+
         # compute similarity with all other cell lines in the training set
         kernels = Vector{Matrix{Float64}}()
-        # dataviews_from_training_idx = findin(training_cell_lines, collect(keys(dep.experiment.results[drug].outcome_values)))
 
         for v in dep.experiment.views
             # dataviews_from_training = map(cl -> cl.views[v], collect(keys(dep.experiment.results[drug].outcome_values)))
-            k = dep.base_kernels[v][training_set_cell_line_idx,predict_cell_line_idx]
-            push!(kernels, k)
+            # k = dep.base_kernels[v][training_set_cell_line_idx,predict_cell_line_idx]
+            push!(kernels, base_cross_kernels[v][training_set_cell_line_idx, :])
             if length(dep.experiment.pathway_information) != 0
-                for pw_kernel in dep.pathway_specific_kernels[v]
-                    push!(kernels, pw_kernel[training_set_cell_line_idx,predict_cell_line_idx])
+                for pw_kernel in pw_specific_cross_kernels[v]
+                    push!(kernels, pw_kernel[training_set_cell_line_idx, :])
                 end
+                # for pw_kernel in dep.pathway_specific_kernels[v]
+                #     push!(kernels, pw_kernel[training_set_cell_line_idx,predict_cell_line_idx])
+                # end
             end
             # @info "computed kernels for view $v, size of kernel: $(size(k))"
         end
 
-        gene_expression = dep.base_kernels[GeneExpression][training_set_cell_line_idx, predict_cell_line_idx]
-        methylation = dep.base_kernels[Methylation][training_set_cell_line_idx, predict_cell_line_idx]
-        cnv = dep.base_kernels[CNV][training_set_cell_line_idx, predict_cell_line_idx]
+        gene_expression = base_cross_kernels[GeneExpression][training_set_cell_line_idx, :]
+        methylation = base_cross_kernels[Methylation][training_set_cell_line_idx, :]
+        cnv = base_cross_kernels[CNV][training_set_cell_line_idx, :]
 
         push!(kernels, gene_expression .* methylation)
         push!(kernels, gene_expression .* cnv)
         push!(kernels, cnv .* methylation)
         push!(kernels, gene_expression .* methylation .* cnv)
 
-        a_expected = expected_value(m.a[t])
-
         G = Vector{Vector{Float64}}(undef, length(kernels))
         exp_a = expected_value(m.a[t])
-        for (k, kernel) in enumerate(kernels)
-            G[k] = kernel' * exp_a
-            # if k == 1
-            #     println("G[$k] : $(typeof(G[k]))")
-            #     display(kernel)
-            # end
-        end
-        # G = transpose.(kernels) .* expected_value(m.a[t]) # results in a vector of scalar intermediate results
-        # println("G: $(size(G)), Gs: $(size.(G))")
-        e_expected = expected_value.(m.e)
-        # println("e: $(size(e_expected)), e type: $(typeof(e_expected)), $e_expected")
-        pred_y = sum(G .* e_expected) .+ expected_value(m.b[t])
+        [G[k] = kernel' * exp_a for (k, kernel) in enumerate(kernels)]
+
+        pred_y = sum(G .* expected_value.(m.e)) .+ expected_value(m.b[t])
 
         #compute ranking of predicted cell line responses
         ranks[drug] = zeros(Int64, length(pred_y))
