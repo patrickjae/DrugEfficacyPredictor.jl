0e7c4272552edd4a60dc352b6bca84e1d1aff74e
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-01-15 15:12:10 +0100

    thread safe logging and some initial global variable declarations

--- init.jl
+++ init.jl
@@ -1,1 +1,18 @@
+using DrugEfficacyPredictor
+
 const PROJECT_ROOT = @__DIR__
+
+# create a global variable to keep track of experiments in the server
+const experiments_dictionary = Dict{String, DrugEfficacyPredictor.Experiment}()
+
+const predictor_dictionary = Dict{String, DrugEfficacyPredictor.DrugEfficacyPrediction}()
+
+# create a job channel
+# const training_jobs = Channel{}
+
+# create a training progress message channel
+const training_progress = Dict{String, Vector{String}}()
+
+const result_file_dictionary = Dict{String, Vector{String}}()
+
+log_message(msg) = Core.println("Thread $(Threads.threadid()): $msg")
