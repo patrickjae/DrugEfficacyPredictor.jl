8490ea34cded0a997456a3caf098b2623b1e55ef
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-01-15 15:17:10 +0100

    automatically load Iorio et al. data via API

--- src/iorio.jl
+++ src/iorio.jl
@@ -1,37 +1,46 @@
-function load_iorio_data()
+function load_iorio_data(;force_reload::Bool = false)
     @async start_server(8888)
     sleep(4)
 
     base_dir = joinpath(PROJECT_ROOT, "data", "iorio")
+    check = haskey(experiments_dictionary, "iorio")
+    if check
+        if force_reload
+            empty!(experiments_dictionary)
+            empty!(training_progress)
+        else
+            log_message("Iorio data set already exists, returning experiment object. If you need to reload, call with parameter force_reload = true")
+            return experiments_dictionary["iorio"]
+        end
+    end
 
     run(`curl -X POST http://localhost:8888/experiments -d '{"experiment_id":"iorio"}'`)
-    @info "check for key" check = haskey(experiments_dictionary, "iorio")
     sleep(2)
     #import the cell lines
     for f in readdir(joinpath(base_dir, "cell_lines"))
         if !startswith(basename(f), "json") continue end
         filename = joinpath(base_dir, "cell_lines", f)
-        cl_cmd = `curl -X POST http://localhost:8888/experiments/iorio/cell_lines -d @$filename`
+        cl_cmd = `curl -X POST http://localhost:8888/experiments/iorio/data/cell_line -d @$filename`
         run(cl_cmd)
     end
 
-    @info "imported cell lines"
+    log_message("imported cell lines")
     sleep(2)
     #import the outcome, use AUC for the time being
     outcome_file = joinpath(PROJECT_ROOT, "data", "iorio", "json_AUCvalues.txt")
-    outcome_add_cmd = `curl -X POST http://localhost:8888/experiments/iorio/outcomes -d @$outcome_file`
+    outcome_add_cmd = `curl -X POST http://localhost:8888/experiments/iorio/data/outcomes -d @$outcome_file`
     run(outcome_add_cmd)
-    @info "imported outcomes"
+    log_message("imported outcomes")
     sleep(2)
 
     # add pathway info
-    pw_file = joinpath(PROJECT_ROOT, "data", "json", "pathway_current.json")
-    run(`curl -X POST http://localhost:8888/experiments/iorio/pathways -d @$pw_file`)
-    @info "imported pathway info"
+    pw_file = joinpath(PROJECT_ROOT, "data", "json", "pathways_current.json")
+    run(`curl -X POST http://localhost:8888/experiments/iorio/data/pathways -d @$pw_file`)
+    log_message("imported pathway info")
     sleep(2)
 
     # stop the server
-    run(`curl http://localhost:8888/stop`)
+    # run(`curl http://localhost:8888/stop`)
 
     experiments_dictionary["iorio"]
 end
