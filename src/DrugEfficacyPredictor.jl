c365d110efa03597d8b5f766d83598112e283cb5
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-01-28 11:57:28 +0100

    refactor init into main module file

--- src/DrugEfficacyPredictor.jl
+++ src/DrugEfficacyPredictor.jl
@@ -1,24 +1,49 @@
 module DrugEfficacyPredictor
+using Distributed
 
-using CSV, DataFrames, SpecialFunctions, Printf,
-	Statistics, LinearAlgebra, Distributed
+server_process = workers()[1]
+data_process = procs()[1]
 
-include("distributional_parameters.jl")
-include("types.jl")
-include("../init.jl")
-include("data_handling.jl")
-include("dream_challenge_data.jl")
-include("iorio.jl")
-include("webservice.jl")
+log_message(msg) = Core.println("Thread $(Threads.threadid()): $msg")
+function set_process_ids(sp::Int64, dp::Int64)
+    global server_process = sp
+    global data_process = dp
+    nothing
+end
+# helpers
+using Printf
 
-include("create.jl")
-include("get.jl")
-include("delete.jl")
-include("model_initialization.jl")
-include("inference.jl")
-include("prediction.jl")
-include("pipeline.jl")
-include("utils.jl")
+using DataStructures
+
+#data
+using CSV, DataFrames
+
+#computations
+using Statistics, LinearAlgebra, SpecialFunctions
+
+
+const PROJECT_ROOT = joinpath(@__DIR__, "..")
+
+include(joinpath(@__DIR__, "distributional_parameters.jl"))
+include(joinpath(@__DIR__, "types.jl"))
+include(joinpath(@__DIR__, "data_handling.jl"))
+include(joinpath(@__DIR__, "dream_challenge_data.jl"))
+include(joinpath(@__DIR__, "iorio.jl"))
+
+include(joinpath(@__DIR__, "model_initialization.jl"))
+include(joinpath(@__DIR__, "inference.jl"))
+include(joinpath(@__DIR__, "prediction.jl"))
+include(joinpath(@__DIR__, "pipeline.jl"))
+include(joinpath(@__DIR__, "utils.jl"))
+
+
+# web service
+using HTTP, JSON, UUIDs
+
+include(joinpath(@__DIR__, "webservice.jl"))
+include(joinpath(@__DIR__, "create.jl"))
+include(joinpath(@__DIR__, "get.jl"))
+include(joinpath(@__DIR__, "delete.jl"))
 
 ########################
 ######### TODO #########
@@ -32,7 +57,4 @@ include("utils.jl")
 # split application and API - OK (partly)
 
 # cross-validation during training
-
-
-
 end # module
