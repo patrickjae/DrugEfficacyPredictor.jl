4e38ce38dc2826b7ea376038035e3635e6327de6
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-01-28 12:02:08 +0100

    run script that starts processes as necessary and a server

---
+++ src/run.jl
@@ -0,0 +1,20 @@
+using Distributed
+
+sp = workers()[1]
+dp = myid()
+
+if nprocs() == nworkers()
+    # no workers have ben started yet, start a process for the server
+    println("adding server process")
+    global sp = addprocs(1)[1]
+end
+
+using DrugEfficacyPredictor
+# # define module on each process
+@everywhere using DrugEfficacyPredictor
+
+#broadcast server process id and data management process id
+@everywhere DrugEfficacyPredictor.set_process_ids($sp, $dp)
+
+# TODO make port configurable
+@spawnat server_process start_server(8888)
