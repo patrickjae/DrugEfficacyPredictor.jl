696559c6fdcfa100468d551fc243b42a00cc0e37
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-02-14 11:43:46 +0100

    remove clutter

--- src/run.jl
+++ src/run.jl
@@ -8,21 +8,4 @@ addprocs(topology=:master_worker)
 end
 DrugEfficacyPredictor.Webservice.start_server(8888)
 
-# sp = workers()[1]
-# dp = myid()
-#
-# if nprocs() == nworkers()
-#     # no workers have ben started yet, start a process for the server
-#     println("adding server process")
-#     global sp = addprocs(1)[1]
-# end
-#
-# using DrugEfficacyPredictor
-# # # define module on each process
-# @everywhere using DrugEfficacyPredictor
-#
-# #broadcast server process id and data management process id
-# @everywhere DrugEfficacyPredictor.set_process_ids($sp, $dp)
-#
-# # TODO make port configurable
-# @spawnat sp DrugEfficacyPredictor.start_server(8888)
+# TODO make port configurable
