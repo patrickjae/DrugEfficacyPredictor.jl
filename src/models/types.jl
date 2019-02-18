08138d47b3382aea9966447088e3789bb6013b9c
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-02-18 21:44:38 +0100

    more elaborate and extensible type system

--- src/models/types.jl
+++ src/models/types.jl
@@ -5,12 +5,10 @@
 ##############################################################################################################
 # abstract type PredictionModel end
 abstract type Model end
-abstract type BMTMKLModel <: Model end
 
 abstract type PredictionModelParameters end
 
 mutable struct InferenceConfiguration
-	model_type::Type{<:Model}
 	convergence_criterion::Float64
 	min_iter::Int64
 	max_iter::Int64
@@ -22,38 +20,20 @@ mutable struct InferenceConfiguration
 	subsume_pathways::Bool
 	do_variance_filtering::Bool
 
-	InferenceConfiguration() = new(BMTMKLModel, 1e-3, 5, 200, "results/", 1, false, false, false, true, true)
+	InferenceConfiguration() = new(1e-3, 5, 200, "results/", 1, false, false, false, true, true)
 end
 
-mutable struct PredictionModel
-	model_type::Type{<:Model}
+mutable struct PredictionModel{M<:Model}
+	# model_type::Type{<:Model}
 	data::Experiment
 	precomputations::Dict{String, Any}
 
-	function PredictionModel(data::Experiment, ic::InferenceConfiguration)
-		m = new()
-		m.model_type = ic.model_type
-		m.data = data
-
-		# make sure that measurements are normalized across cell lines
-		if !m.data.is_normalized
-			normalize_data_views(m.data)
-		end
-
-		#do variance filtering if required
-		if ic.do_variance_filtering
-			filter_data_views(m.data)
-		else
-			# make sure we are using all keys
-			unfilter_data_views(m.data)
-		end
-
-		m.precomputations = Dict{String, Any}()
-		return init(m, ic)
-	end
+	PredictionModel(::Type{M}, data::Experiment) where {M <: Model} = new{M}(data, Dict{String, Any}())
 end
 
 struct ModelConfiguration
 	parameters::Dict{String, Any}
-	ModelConfiguration() = new(Dict{String, Any}())
+	function ModelConfiguration()
+		new(Dict{String, Any}())
+	end
 end
