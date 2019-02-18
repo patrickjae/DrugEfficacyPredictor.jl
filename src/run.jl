09bf7296b76ebcdaabfab0fc1be881201c356232
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-02-18 21:50:12 +0100

    configurable num of workers, set threads to num CPUs, only use code needed

--- src/run.jl
+++ src/run.jl
@@ -1,11 +1,25 @@
+# set the number of worker threads for managing experiments
+num_worker_procs = length(ARGS) == 0 ? 2 : parse(Int64, ARGS[1])
+# setting the number of threads to number of CPUs
+# this is default behavior, setting it anyway to override if set otherwise
+ENV["JULIA_NUM_THREADS"] = Sys.CPU_THREADS
+# start the workers on which experiments are loaded
 using Distributed
-addprocs(topology=:master_worker)
+addprocs(num_worker_procs, topology=:master_worker)
+w = workers()
 
 #make sure we are able to load everything
 @everywhere begin
+    Core.println("loading code")
     import Pkg; Pkg.activate(".")
     using DrugEfficacyPredictor
+    Core.println("done loading code")
 end
+
+# make sure workers now their ids
+@everywhere DrugEfficacyPredictor.Utils.init_workers($w)
+
+# start the webservice
 DrugEfficacyPredictor.Webservice.start_server(8888)
 
 # TODO make port configurable
