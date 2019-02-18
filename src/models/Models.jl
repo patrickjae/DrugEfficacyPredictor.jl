0d6b25c65fbd78d003e41a37eb6229849e55f5ff
Patrick Jähnichen <patrick.jaehnichen@hu-berlin.de>
2019-02-18 21:42:05 +0100

    more using statements, remove comments, slightly change include order

--- src/models/Models.jl
+++ src/models/Models.jl
@@ -1,18 +1,18 @@
 module Models
 #computations
-using Statistics, LinearAlgebra, SpecialFunctions, Distributions, Printf, Distributed
+using Statistics, LinearAlgebra, SpecialFunctions, Distributions, Printf, Distributed, DataStructures
 import ..Utils: log_message, log_progress, PROJECT_ROOT
 import ..Data: Experiment, CellLine, Drug, Gene, Protein, RNASeqCall, ExomeSeq, RPPA, Pathway
 import ..Data: DataView, KeyType, ViewType, ValueViewType, NAViewType
-import ..Data: get_experiment, get_measurement_value
-# Core.println("loading submodule Models on process $(myid()): $(@__MODULE__)")
-
+import ..Data: get_experiment, get_measurement_value, normalize_data_views, filter_data_views, unfilter_data_views
+using ..Utils, ..Data
 include("distributional_parameters.jl")
 include("types.jl")
-include("bmtmkl.jl")
+include("utils.jl")
+include("bmtmkl/bmtmkl.jl")
+# <======== additional models need to be included here
 include("run_models.jl")
 include("io.jl")
-include("utils.jl")
 include("pipeline.jl")
 
 
